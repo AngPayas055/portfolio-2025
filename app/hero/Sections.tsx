a6dfac14f61b1eb53d820441e7a38d3cc06e4b4c
Jhon Rey Ruiz Mendiola <jhon@draftal.com.au>
2025-09-11 14:40:29 +0800

    font size update

--- app/hero/Sections.tsx
+++ app/hero/Sections.tsx
@@ -7,7 +7,7 @@ export default function Sections() {
     <div className="pt-20 md:w-7/12 ">
       <About></About>
       <div className="mt-10 space-y-6">
-        <h3 className="sticky top-0 px-3 bg-[var(--color-bg)] pt-5 pb-5 z-10 text-[var(--color-text)]">Experience</h3>
+        <h5 className="sticky top-0 px-3 bg-[var(--color-bg)] pt-5 pb-5 z-10 text-[var(--color-text)] text-xs">Experience</h5>
         <div className="px-1">
           <ExperienceList></ExperienceList>
         </div>
